af58fce8c937fda3badc7d67aa100b78ea569f4e
prativapokhrel <prativa@vayu.com.au>
2019-04-15 22:53:15 +0545

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'api/v1/users#index'
+
   namespace :api do 
     namespace :v1 do 
       resources :users 
